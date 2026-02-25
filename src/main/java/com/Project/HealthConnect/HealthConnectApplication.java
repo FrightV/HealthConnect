ac469662936ffe1aceb9ffa57649f913e28b5853
FrightV <victorderumbutis@gmail.com>
2026-02-24 22:39:59 -0300

    first commit

---
+++ src/main/java/com/Project/HealthConnect/HealthConnectApplication.java
@@ -0,0 +1,14 @@
+package com.Project.HealthConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthConnectApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/Project/HealthConnect/HealthConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project.HealthConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
