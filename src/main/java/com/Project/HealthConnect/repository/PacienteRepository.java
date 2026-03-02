02b859534da69298e62fef8408703d0e49ef87d0
FrightV <victorderumbutis@gmail.com>
2026-03-02 16:10:58 -0300

    criação do PacienteRepository.

---
+++ src/main/java/com/Project/HealthConnect/repository/PacienteRepository.java
@@ -0,0 +1,7 @@
+package com.Project.HealthConnect.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.Project.HealthConnect.model.Paciente;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+}
