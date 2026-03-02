c6f6d82864432fd4de506cb92a8a939381a649ea
FrightV <victorderumbutis@gmail.com>
2026-03-02 16:10:27 -0300

    criação da entidade Paciente.

---
+++ src/main/java/com/Project/HealthConnect/model/Paciente.java
@@ -0,0 +1,26 @@
+package com.Project.HealthConnect.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "pacientes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    private String telefone;
+
+    private String email;
+}
