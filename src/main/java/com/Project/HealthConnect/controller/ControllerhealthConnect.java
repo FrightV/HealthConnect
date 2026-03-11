b480be8fe80f59234aa2ad1f0fb18e4ae6940ea6
FrightV <victorderumbutis@gmail.com>
2026-03-11 14:00:36 -0300

    correção da estrutura

--- src/main/java/com/Project/HealthConnect/controller/ControllerhealthConnect.java
+++ src/main/java/com/Project/HealthConnect/controller/ControllerhealthConnect.java
@@ -1,4 +1,4 @@
-package com.Project.HealthConnect;
+package com.Project.HealthConnect.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
